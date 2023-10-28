055d3894bbe512039e2a996c408e54941cd3b811
zhims <jaafar_zhang@163.com>
2023-10-28 20:13:03 +0800

    submit

---
+++ SoPh2023b/AISolver/LL1990/1/demo_1_1.m
@@ -0,0 +1,25 @@
+% Y. Zhang
+% https://zhims.github.io/
+% Oct. 28, 2023
+
+load  para_1.mat
+
+t0 = -1;
+t1 = 1;
+N = 10^4-1;
+h = (t1 - t0)/N;
+t_Test = t0:h:t1;
+numPredictions = length(t_Test);
+
+figure
+t_Test = dlarray(t_Test,"CB");
+F_Fprime_Pred = model(parameters,t_Test);
+
+subplot(1,2,1)
+plot(t_Test,extractdata(F_Fprime_Pred(1,:)),"-",LineWidth=2);
+title('F')
+
+subplot(1,2,2)
+plot(t_Test,extractdata(F_Fprime_Pred(2,:)),"-",LineWidth=2);
+title('F^{\prime}')
+
